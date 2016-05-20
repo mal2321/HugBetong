7db1850ced1bf33090946541a33f182d58d91b15
Hamal <hamalsaha@gmail.com>
2016-05-20 22:20:47 +0700

    20/05/16

--- app/src/main/java/com/project/jasnawa/hugbetong/MainActivity.java
+++ app/src/main/java/com/project/jasnawa/hugbetong/MainActivity.java
@@ -1,31 +1,25 @@
 package com.project.jasnawa.hugbetong;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    public DrawerLayout drawerLayout;
+    public CoordinatorLayout rootLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
+
+
     }
 
     @Override
